c48a5afb874ba5070bc0c8ca0472b38ead7f493f
Cristian A Rennella <cristianarennella@Cristians-MacBook-Pro.local>
2018-02-27 18:56:52 -0300

    Add Votes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def fix_url(str)
+		str.starts_with?('http://') ? str: "http://#{str}"
+	end
+
+	def display_datetime(date)
+		date.strftime("on %m/%d/%Y at %I:%M%p")
+	end
 end
---
+++ app/helpers/posts_helper.rb
@@ -0,0 +1,5 @@
+module PostsHelper
+	def capitalize(str)
+		str.titleize
+	end
+end
